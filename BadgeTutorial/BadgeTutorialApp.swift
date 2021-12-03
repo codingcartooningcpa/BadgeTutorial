7d19a0fa42f9bf5cffa15e152be710a921e8f9b7
Debra Sackheim Kerman <debra678@gmail.com>
2021-12-03 08:53:37 -0600

    Initial Commit

---
+++ BadgeTutorial/BadgeTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadgeTutorialApp.swift
+//  BadgeTutorial
+//
+//  Created by Debra Kerman on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct BadgeTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
